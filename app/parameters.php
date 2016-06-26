248c5dbfde2bf2010bb13d461359e3e286ad2698
nicolas_longuet <nicolas.longuet@vivaneo.fr>
2016-06-26 22:16:55 +0200

    first

---
+++ app/parameters.php
@@ -0,0 +1,12 @@
+<?php
+
+$parameters = [
+    'domaine' => '/phpform/all/Projet/Framework/web/app.php',
+    'pdo' => [
+        'database_host'     => 'localhost',
+        'database_name'     => 'phpform',
+        'database_user'     => 'root',
+        'database_password' => '',
+    ],
+];
+
